7c9ac783c3eaabaf9e70c42e45969f716da36e12
Devan Mathis <d130m536@home.ku.edu>
2026-09-23 10:30:52 -0500

    Started Lab 4

--- NFL_score.c
+++ NFL_score.c
@@ -1,5 +1,28 @@
 #include stdio
 
+void sum_points(int nums[], int target, int partial[] = []) {
+	sum = sum(partial, sizeof(partial) / sizeof(partial[0]);
+
+	if sum == target {
+		printf("Sum go here after counting"); }
+	if sum > target { return; }
+	/*
+	for i in range(sizeof(nums) / sizeof(nums[0]) {
+		int n = nums[i];
+	}
+	*\	
+}
+
+int sum(int arr[], int size){
+	int total = 0;
+	
+	for (int i = 0; i < size; i++) {
+		total += arr[i];	
+	}
+	return total;
+}
+
 int main() {
 	printf("It should be called football, not soccer.");
+	
 }
